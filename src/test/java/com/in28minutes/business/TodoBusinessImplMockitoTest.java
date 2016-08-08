297919642ff0fa1d1f55c5dcd3b94fd4e0ccd79a
Ranga Rao Karanam <rangaraokaranam@Rangas-MacBook-Pro.local>
2016-08-08 09:37:26 +0530

    Step8

--- src/test/java/com/in28minutes/business/TodoBusinessImplMockitoTest.java
+++ src/test/java/com/in28minutes/business/TodoBusinessImplMockitoTest.java
@@ -12,6 +12,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
 import com.in28minutes.data.api.TodoService;
@@ -71,5 +72,23 @@ public class TodoBusinessImplMockitoTest {
 		verify(todoService, Mockito.times(1)).deleteTodo("Learn to Dance");
 		// atLeastOnce, atLeast
 
+	}
+
+	@Test
+	public void captureArgument() {
+		ArgumentCaptor<String> argumentCaptor = ArgumentCaptor
+				.forClass(String.class);
+
+		TodoService todoService = mock(TodoService.class);
+
+		List<String> allTodos = Arrays.asList("Learn Spring MVC",
+				"Learn Spring", "Learn to Dance");
+		Mockito.when(todoService.retrieveTodos("Ranga")).thenReturn(allTodos);
+
+		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoService);
+		todoBusinessImpl.deleteTodosNotRelatedToSpring("Ranga");
+		Mockito.verify(todoService).deleteTodo(argumentCaptor.capture());
+
+		assertEquals("Learn to Dance", argumentCaptor.getValue());
 	}
 }
